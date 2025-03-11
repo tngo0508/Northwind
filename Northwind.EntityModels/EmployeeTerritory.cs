ecdb4ded3f6830ecd52c59e69fb4cf29fdeb13cf
Thomas Ngo <tngo@ochca.com>
2025-03-11 15:45:27 -0700

    Add entity models

---
+++ Northwind.EntityModels/EmployeeTerritory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Northwind.EntityModels;
+
+[Keyless]
+public partial class EmployeeTerritory
+{
+    public int EmployeeId { get; set; }
+
+    [Column("TerritoryID")]
+    [StringLength(20)]
+    public string TerritoryId { get; set; } = null!;
+
+    [ForeignKey("EmployeeId")]
+    public virtual Employee Employee { get; set; } = null!;
+
+    [ForeignKey("TerritoryId")]
+    public virtual Territory Territory { get; set; } = null!;
+}
---
+++ Northwind.EntityModels/Territory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace Northwind.EntityModels;
+
+public partial class Territory
+{
+    [Key]
+    [Column("TerritoryID")]
+    [StringLength(20)]
+    public string TerritoryId { get; set; } = null!;
+
+    [StringLength(50)]
+    public string TerritoryDescription { get; set; } = null!;
+
+    [Column("RegionID")]
+    public int RegionId { get; set; }
+
+    [ForeignKey("RegionId")]
+    [InverseProperty("Territories")]
+    public virtual Region Region { get; set; } = null!;
+}
