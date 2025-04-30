f33033d8cf062e6cc43558466ef76ec11ca4e04e
Thomas Ngo <tngo0508@gmail.com>
2025-04-29 21:03:26 -0700

    Update WebApi project with new dependencies and console outputs
    Added a project reference to Northwind.DataContext and included static `System.Console` usage. Enhanced controllers with detailed output formatter logging and support for XML formatters. Adjusted JetBrains Rider settings to include specific files for code inspection.

--- Northwind.WebApi/Program.cs
+++ Northwind.WebApi/Program.cs
@@ -1,8 +1,32 @@
+using Microsoft.AspNetCore.Mvc.Formatters; // To use IOutputFormatter.
+using Northwind.EntityModels; // To use AddNorthwindContext method.
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddNorthwindContext();
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    WriteLine("Default output formatters:");
+    foreach (var formatter in options.OutputFormatters)
+    {
+        OutputFormatter? mediaFormatter = formatter as OutputFormatter;
+        if (mediaFormatter is null)
+        {
+            WriteLine($"   {formatter.GetType().Name}");
+        }
+        else
+        {
+            WriteLine("    {0}, Media types: {1}",
+                arg0: mediaFormatter.GetType().Name,
+                arg1: string.Join(", ", mediaFormatter.SupportedMediaTypes)
+            );
+        }
+    }
+})
+.AddXmlDataContractSerializerFormatters()
+.AddXmlSerializerFormatters();
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
